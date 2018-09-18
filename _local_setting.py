05c45426a6da8cabf1edf0347d608c8de7e47823
Vlad <vaindante@yandex.ru>
2018-09-18 21:35:31 +0500

    тестовое задание

--- _local_setting.py
+++
@@ -1,6 +0,0 @@
-settings = dict(
-    host_api='https://auth.iqoption.com/api/v1.0',
-    host_ws='wss://iqoption.com/echo/websocket',
-    passwd='hed01351@bnuis',
-    email='hed01351@bnuis.com'
-)
